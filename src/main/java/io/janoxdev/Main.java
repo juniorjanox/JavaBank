00ef09907357cd55be6f22f4ccf48e90d62d967f
juniorjanox <juniorjanox@gmail.com>
2024-11-03 11:15:59 -0500

    JavaBank

---
+++ src/main/java/io/janoxdev/Main.java
@@ -0,0 +1,22 @@
+package io.janoxdev;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                if (i == 5) {
+                    throw new AlcanzoLimite("El for llegó a su límite en " + i);
+                }
+                System.out.println(i);
+            }
+        } catch (AlcanzoLimite e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class AlcanzoLimite extends Exception {
+    public AlcanzoLimite(String message) {
+        super(message);
+    }
+}
